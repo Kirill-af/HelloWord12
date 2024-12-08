062ae68dc6ffecec75f902c9f7ad5d9ccd060c9f
kirill <kirillstefanov423@gmail.com>
2024-12-08 20:55:05 +0600

    Object Methods

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Author ekkel = new Author("Брюс", "Борис");
+        Author martin = new Author("Робкрт", "Николай");
+
+
+        Book javaPhilosophy = new Book("Философия Java", ekkel, 2017);
+        Book cleanCode = new Book("Чистый код", martin, 2012);
+
+        System.out.println(javaPhilosophy.getTitle() + " " + javaPhilosophy.getPublicationYear());
+
+        javaPhilosophy.setPublicationYear(2024);
+
+        System.out.println(javaPhilosophy.getTitle() + " " + javaPhilosophy.getPublicationYear());
+    }
+}
